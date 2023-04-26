de8d16785b91c2bd212ba71d7a98e21f3254e2d6
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2023-04-26 22:24:04 +0100

    update taask l0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,26 +8,15 @@ import sys
 
 
 if __name__ == '__main__':
-    employee_Id = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employee_Id
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    response = requests.get(url)
-    employee_Name = response.json().get('name')
+    user_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
+    todo = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    done = 0
-    completed_tasks = []
-
-    for task in tasks:
-        if task.get('completed'):
-            completed_tasks.append(task)
-            done += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_Name, done, len(tasks)))
-
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
+    completed = [title.get("title") for title in todo if
+                 title.get('completed') is True]
+    print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(user_id.get("name"),
+                                                          len(completed),
+                                                          len(todo)))
+    [print("\t {}".format(title)) for title in completed]
