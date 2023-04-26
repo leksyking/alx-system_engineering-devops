bf3ab242f28c2d954da14c5a9663dd54ec451a63
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2023-04-26 21:03:18 +0100

    add task 1, to export data in csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Export data in CSV format
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employee_Id = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employee_Id
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employee_Id), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employee_Id, username, task.get('completed'),
+                               task.get('title')))
