4a9f563b3df41fc5670a43ddf88d38c3196a3a2b
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2023-05-31 05:00:41 +0100

    update the code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,6 @@
-# After finding out why Apache is returning a 500 error
 # Here is a puppet manuscript to replace a line in a file on a server
 
 $file_to_edit = '/var/www/html/wp-settings.php'
-
-#replace line containing "phpp" with "php"
 
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' ${file_to_edit}",
